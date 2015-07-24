5368b04878277994464999f5b906598b30db0d91
Fabien Poussin <fabien.poussin@gmail.com>
2015-07-24 15:40:07 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,9 +1,25 @@
+@echo off
+setlocal
 
-git clone -b 5.5 https://github.com/qtproject/qtbase.git
-cd qtbase
+set BUILDDIR="build"
+set QTVER="5.5"
+set OPENSSL="C:\OpenSSL-Win32"
 
-cp -r ..\win32-msvc2012-static mkspecs\
+git clone -b %QTVER% https://github.com/qtproject/qtbase.git qtbase-%QTVER%
+cd qtbase-%QTVER%
 
-mkdir build
-cd build
-..\configure.exe -prefix C:\Qt\5.5.0-static -platform win32-msvc2012-static -opensource -release -opengl desktop -mp -static -nomake examples -qt-libpng -qt-libjpeg -qt-zlib -qt-pcre -openssl -no-angle -no-accessibility -I C:\OpenSSL-Win32\include -L C:\OpenSSL-Win32\lib
+xcopy /e /c /i /k /y ..\win32-msvc2012-static mkspecs\
+
+echo "Cleaning old build dir"
+rd /s /q %BUILDDIR%
+
+md %BUILDDIR%
+cd %BUILDDIR%
+..\configure.exe -prefix C:\Qt\%QTVER%-static -platform win32-msvc2012-static -opensource -release -opengl desktop -mp -static -nomake examples -qt-libpng -qt-libjpeg -qt-zlib -qt-pcre -openssl -no-angle -no-accessibility -I %OPENSSL%\include -L %OPENSSL%\lib
+
+echo "Configuration complete"
+echo "Will install to C:\Qt\%QTVER%-static"
+echo "Type nmake to build"
+echo "Type nmake install to install"
+
+endlocal
