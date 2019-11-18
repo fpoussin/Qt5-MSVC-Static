8add11fba7be8070cf429f1f584c3e583311d691
Fabien Poussin <fabien.poussin@gmail.com>
2019-11-18 10:54:37 +0100

    Update options.bat

--- tools/options.bat
+++ tools/options.bat
@@ -2,8 +2,8 @@
 
 
 set MSVCVER=2017
-set QTVER=5.13.0
-set SSLVER=1.1.1c
+set QTVER=5.13.2
+set SSLVER=1.1.1d
 set PREFIX=C:\Qt
 set EXTRABUILDOPTIONS=-qt-sqlite
 set PATH=C:\Python\;C:\Python27\;C:\Strawberry\perl\bin;%PATH%
